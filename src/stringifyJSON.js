3f90dfa7c0a96b2203632eef3d650fb31cfa95b5
Simon Stroh <simon_stroh@outlook.com>
2018-04-17 19:45:10 -0400

    Changes made for the stringify document

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,12 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if (typeof obj === "object") {
+  if (Array.isArray(obj)) {
+    var specialArray = []
+    for (r = 0; r < obj.length; r++) {
+      if (typeof obj[r] === "string") {
+        var string4 = '"' + obj[r] + '"'
+        specialArray.push(string4)
+      } else if (typeof obj[r] === "boolean" || typeof obj[r] === "number") {
+        var string4 = obj[r]
+        specialArray.push(string4)
+      } else if (typeof obj[r] === "undefined") {
+        var string4 = 'null'
+        specialArray.push(string4)
+      }
+    }
+    var arrayString = specialArray.join(',')
+    return '[' + arrayString + ']'
+  } else if (typeof obj === "object") {
     var newArray = []
     for (property in obj) {
       var newString = property
-      var otherNewString = obj[property]
       newArray.push(newString)
+      var otherNewString = obj[property]
       newArray.push(otherNewString)
     }
     if (newArray.length === 2) {
@@ -19,19 +35,41 @@ var stringifyJSON = function(obj) {
       var string1 = '{'
       specialArray.push(string1)
       for (i = 0; i < newArray.length; i++) {
-        if (i === 0 || newArray.indexOf(newArray[i]) % 2 === 0) {
-          var string2 = '"' + newArray[i] + '"' + ':'
+        if (i === 0 || i % 2 === 0) {
+          var string2 = '"' + newArray[i] + '":'
           specialArray.push(string2)
-        } else if (i >= 1 && newArray.indexOf(newArray[i]) % 2 !== 0 && i !== newArray.length - 1) {
-          var string3 = newArray[i] + ','
-          specialArray.push(string3)
+        } else if (i >= 1 && i % 2 !== 0 && i !== newArray.length - 1) {
+          if (typeof newArray[i] === "string") {
+            var string3 = '"' + newArray[i] + '",'
+            specialArray.push(string3)
+          } else if (typeof newArray[i] === "boolean" || typeof newArray[i] === "number") {
+            var string3 = newArray[i] + ','
+            specialArray.push(string3)
+          } else if (typeof newArray[i] === "undefined") {
+            var string3 = null + ','
+            specialArray.push(string3)
+          }
         } else if (i === newArray.length - 1) {
-          var string4 = newArray[i] + '}'
-          specialArray.push(string4)
+          if (typeof newArray[i] === "string") {
+            var string3 = '"' + newArray[i] + '"}'
+            specialArray.push(string3)
+          } else if (typeof newArray[i] === "boolean" || typeof newArray[i] === "number") {
+            var string3 = newArray[i] + '}'
+            specialArray.push(string3)
+          } else if (typeof newArray[i] === "undefined") {
+            var string3 = null + '}'
+            specialArray.push(string3)
+          }
         }
       }
       var returnString = specialArray.join('')
       return returnString
     }
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"'
+  } else if (typeof obj === "boolean" || typeof obj === "number") {
+    return obj
+  } else if (typeof obj === "undefined") {
+    return null
   }
 };
