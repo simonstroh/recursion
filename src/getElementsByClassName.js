ebaa664f825810fec7d43c8497fdee05f984796a
Simon Stroh <simon_stroh@outlook.com>
2018-04-17 20:39:42 -0400

    Changes for the last minute

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,12 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var documentElements = document.body.childNodes
+  var newArray = []
+  for (r = 0; r < documentElements.length; r++) {
+    if (documentElements[r].contains(className)) {
+      newArray.push(documentElements[r])
+    }
+  }
+  return newArray
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -16,6 +16,74 @@ var stringifyJSON = function(obj) {
       } else if (typeof obj[r] === "undefined") {
         var string4 = 'null'
         specialArray.push(string4)
+      } else if (Array.isArray(obj[r])) {
+        var thisArray = obj[r]
+        var concatArray = []
+        for (t = 0; t < thisArray.length; t++) {
+          if (typeof thisArray[t] === "string") {
+            var stringHere = '"' + thisArray[t] + '"'
+            concatArray.push(stringHere)
+          } else if (typeof thisArray[t] === "boolean" || typeof thisArray[t] === "number") {
+            var stringHere = thisArray[t]
+            concatArray.push(stringHere)
+          } else if (typeof thisArray[t] === "undefined") {
+            var stringHere = 'null'
+            concatArray.push(stringHere)
+          } else if (Array.isArray(thisArray[t])) {
+            if (thisArray[t].length === 0) {
+              var stringHere = '[]'
+              concatArray.push(stringHere)
+            } else {
+              var deeperArray = thisArray[t]
+              var deepConcatArray = []
+              for (x = 0; x < deeperArray.length; x++) {
+                if (typeof deeperArray[x] === "string") {
+                  var thisOne = '"' + deeperArray[x] + '"'
+                  deepConcatArray.push(thisOne)
+                } else if (typeof deeperArray[x] === "boolean" || typeof deeperArray[x] === "number") {
+                  var thisOne = deeperArray[x]
+                  deepConcatArray.push(thisOne)
+                } else if (typeof deeperArray[x] === "undefined") {
+                  var thisOne = 'null'
+                  deepConcatArray.push(thisOne)
+                } else if (Array.isArray(deeperArray[x])) {
+                  if (deeperArray[x].length === 0) {
+                    var thisOne = '[]'
+                    deepConcatArray.push(thisOne)
+                  } else {
+                    var deepestArray = deeperArray[x]
+                    var deepestConcatArray = []
+                    for (y = 0; y < deepestArray.length; y++) {
+                      if (typeof deepestArray[y] === "string") {
+                        var thisHere = '"' + deepestArray[y] + '"'
+                        deepestConcatArray.push(thisHere)
+                      } else if (typeof deepestArray[y] === "boolean" || typeof deepestArray[y] === "number") {
+                        var thisHere = deeperArray[x]
+                        deepestConcatArray.push(thisHere)
+                      } else if (typeof deepestArray[y] === "undefined") {
+                        var thisHere = 'null'
+                        deepestConcatArray.push(thisHere)
+                      } else if (Array.isArray(deepestArray[y])) {
+                        if (deepestArray[y].length === 0) {
+                          var thisHere = '[]'
+                          deepestConcatArray.push(thisHere)
+                        } else {
+
+                        }
+                      }
+                    }
+                    var returningStringLast = '[' + deepestConcatArray.join(',') + ']'
+                    deepConcatArray.push(returningStringLast)
+                  }
+                }
+              }
+              var returningString = '[' + deepConcatArray.join(',') + ']'
+              concatArray.push(returningString)
+            }
+          }
+        }
+        var thisString = '[' + concatArray.join(',') + ']'
+        specialArray.push(thisString)
       }
     }
     var arrayString = specialArray.join(',')
@@ -64,12 +132,16 @@ var stringifyJSON = function(obj) {
       }
       var returnString = specialArray.join('')
       return returnString
+    } else if (newArray.length === 0) {
+      return '{}'
     }
   } else if (typeof obj === "string") {
     return '"' + obj + '"'
   } else if (typeof obj === "boolean" || typeof obj === "number") {
-    return obj
+    return obj.toString()
   } else if (typeof obj === "undefined") {
-    return null
+    return 'null'
+  } else {
+    return 'null'
   }
 };
